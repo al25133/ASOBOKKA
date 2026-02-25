1d598e31335804c636f7e3314575e6d76ead734e
Atuy <130917473+atuy1219@users.noreply.github.com>
2026-02-25 01:58:37 +0000

    ビルドエラーを修正

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,19 @@
 import type { NextConfig } from "next";
-import withPWA from "next-pwa";
+import nextPwa from "next-pwa";
 import runtimeCaching from "next-pwa/cache";
+
+const withPWA = nextPwa({
+  dest: "public",
+  runtimeCaching,
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    appDir: true,
-  },
 };
 
 export default withPWA({
   ...nextConfig,
-  pwa: {
-    dest: "public",
-    runtimeCaching,
-    disable: process.env.NODE_ENV === "development",
-    register: true,
-    skipWaiting: true,
-  },
 });
